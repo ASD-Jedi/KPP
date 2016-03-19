41b29860d55c71760e29785f0efc828057ab32f4
Alexander Shulyak <venator95@mail.ru>
2016-03-20 00:23:48 +0300

    add

---
+++ Main.java
@@ -0,0 +1,31 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setHeight(400);
+        primaryStage.setWidth(200);
+
+        primaryStage.setResizable(false);
+        Settings set = new Settings();
+
+
+        System.out.println(primaryStage.getHeight());
+        System.out.println(primaryStage.getWidth());
+        primaryStage.setTitle("River - Settings");
+
+        primaryStage.setScene(set.SettingsScene(primaryStage));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
---
+++ Settings.java
@@ -0,0 +1,123 @@
+package sample;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class Settings {
+    private int Hight;
+    private int Wight;
+    private int screenH;
+    private int screenW;
+    private Controller MainLogic;
+    private Pane local;
+    private Rectangle baccker;
+    private Button setter;
+    private Label ResLabel;
+    private Label ScaleLabel;
+    private ComboBox<String> Resolution;
+    private ComboBox<String> Scale;
+
+    private void interfaceInit() {
+        baccker.setHeight(400);
+        baccker.setWidth(200);
+        baccker.setX(0);
+        baccker.setY(0);
+        baccker.setFill(Paint.valueOf("#BFBFBF"));
+
+        ResLabel.setLayoutX(55);
+        ResLabel.setLayoutY(10);
+        ResLabel.setPrefSize(200, 20);
+        ResLabel.setText("Set Resolution");
+        ResLabel.setTextFill(Paint.valueOf("Black"));
+        ResLabel.setTextAlignment(TextAlignment.CENTER);
+
+        setter.setText("Accept Settings");
+        setter.setPrefSize(130, 20);
+        setter.setTranslateX(30);
+        setter.setTranslateY(350);
+
+        Resolution.getItems().addAll(
+                "640x480",
+                "800x600",
+                "1024x768",
+                "1280x720",
+                "1366x768"
+        );
+        Resolution.setPrefSize(150, 30);
+        Resolution.setTranslateX(30);
+        Resolution.setTranslateY(40);
+
+    }
+
+    public Settings() {
+        local = new Pane();
+        baccker = new Rectangle();
+        setter = new Button();
+        ResLabel = new Label();
+        ScaleLabel = new Label();
+        Resolution = new ComboBox<>();
+        Scale = new ComboBox<>();
+        MainLogic = new Controller();
+        GraphicsDevice getRes = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        screenH = getRes.getDisplayMode().getHeight();
+        screenW = getRes.getDisplayMode().getWidth();
+    }
+
+    public Scene SettingsScene(Stage edit) {
+        interfaceInit();
+        setter.setOnAction(event -> {
+            if (Resolution.getValue() != null) {
+                switch (Resolution.getValue()) {
+                    case "640x480":
+                        Wight = 640;
+                        Hight = 480;
+                        break;
+                    case "800x600":
+                        Wight = 800;
+                        Hight = 600;
+                        break;
+                    case "1024x768":
+                        Wight = 1024;
+                        Hight = 768;
+                        break;
+                    case "1280x720":
+                        Wight = 1280;
+                        Hight = 720;
+                        break;
+                    case "1366x768":
+                        Wight = 1366;
+                        Hight = 768;
+                        break;
+                    default:
+                        System.out.println("Doesn't work");
+                        break;
+                }
+            } else {
+                Wight = 800;
+                Hight = 600;
+            }
+            MainLogic.sizeSet(Hight, Wight);
+
+            edit.setScene(MainLogic.sceneController());
+            edit.setX(screenH / 2 - 800 / 2);
+            edit.setY(screenW / 2 - 600 / 2);
+            edit.setWidth(Wight);
+            edit.setHeight(Hight);
+            edit.show();
+        });
+
+        local.getChildren().addAll(baccker, ResLabel, setter, Resolution);
+        Scene chose = new Scene(local);
+        return chose;
+    }
+
+}
